5f3b9c9b09db58d4c461b79d05e9ee1b5712ca35
Marc Mignot <marc.migot@gmail.com>
2021-07-06 21:16:18 +0100

    added fancy

--- model.py
+++ model.py
@@ -12,19 +12,22 @@ def get_flags_passed_in_from_terminal():
 args = get_flags_passed_in_from_terminal()
 print(args.model)
 
-models = {
-    'Linear': linear_model.LinearRegression(),
-    'Ridge': linear_model.Ridge(),
-    'Lasso': linear_model.Lasso(),
-    'ElasticNet': linear_model.ElasticNet(),
-    'Tweedee': linear_model.TweedieRegressor()
-}
+mlflow.set_tracking_uri("file:/C:\Users\marcm\mlruns")
 
+# models = {
+#     'Linear': linear_model.LinearRegression(),
+#     'Ridge': linear_model.Ridge(),
+#     'Lasso': linear_model.Lasso(),
+#     'ElasticNet': linear_model.ElasticNet(),
+#     'Tweedee': linear_model.TweedieRegressor()
+# }
+
+model = getattr(linear_model, args.model)
 
 X, y = load_boston(return_X_y = True)
 
 with mlflow.start_run():
-    model = models[args.model]
+    # model = models[args.model]
     model.fit(X, y)
     
     score = model.score(X, y)
